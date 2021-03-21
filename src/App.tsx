01566ba190c469b05493b1ed352702cdf7c5e91a
spiddy <dimitris.kapanidis@harbur.io>
2021-03-21 14:16:44 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
       <header className="App-header">
         <div className="font-sans antialiased h-screen flex">
           <div className="bg-indigo-darkest text-purple-lighter flex-none w-22 p-6 hidden md:block">
-            <WorkspaceButton title="Kubernetes" id={1} />
-            <WorkspaceButton title="Generic" id={2} />
+            <WorkspaceButton title="Kubernetes" shortcut={1} />
+            <WorkspaceButton title="Generic" shortcut={2} />
             <div className="cursor-pointer">
               <div className="bg-white opacity-25 h-10 w-10 flex items-center justify-center text-black text-2xl font-semibold rounded-lg mb-1 overflow-hidden">
                 <svg className="fill-current h-10 w-10 block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z" /></svg>
--- src/WorkspaceButton.tsx
+++ src/WorkspaceButton.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
 
-interface Props { title: string, id: number }
-function WorkspaceButton({ title, id }: Props) {
+interface Props { title: string, shortcut: number }
+function WorkspaceButton({ title, shortcut }: Props) {
   return (
-    <div className="cursor-pointer mb-5">
+    <div data-tip data-for={`${shortcut}`} className="cursor-pointer mb-5">
       <div className="bg-gray-500 h-10 w-10 flex items-center justify-center text-black text-2xl font-semibold rounded-lg mb-1 overflow-hidden hover:ring-4 ring-offset-4 ring-offset-current ring-gray-400">
         {title[0]}
       </div>
+      <ReactTooltip offset={{ right: 2 }} id={`${shortcut}`} className="tooltip" place="right" effect="solid" delayShow={400} border={false} backgroundColor="transparent">
+        <div className="bg-gray-800 p-1 border rounded-md border-gray-700 px-2 text-2xl text-gray-300 font-light">
+          {title}&nbsp;&nbsp;&nbsp;
+          <span className="text-gray-200 text-lg font-extralight"> <code className="bg-gray-700 inset-0 px-1 rounded-md ">&#8984;</code> <code className="bg-gray-700 inset-0 px-1 rounded-md">{shortcut}</code></span>
+        </div>
+      </ReactTooltip>
     </div>
   )
 }
