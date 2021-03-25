0e678a2a9679613019952c15d8c4da71574dd6f3
spiddy <dimitris.kapanidis@harbur.io>
2021-03-25 15:22:09 +0100

    wip

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,12 +1,10 @@
 import { Avatar } from '@material-ui/core';
-import { ArrowBack, ArrowForward, HelpOutline, PersonOutline, QueryBuilder, Search } from '@material-ui/icons';
+import { ArrowBack, ArrowForward, HelpOutline, QueryBuilder, Search } from '@material-ui/icons';
 import React from 'react';
 import { useStateValue } from '../StateProvider';
 
-interface TopMenuProps { title: string }
-function TopMenu({ title }: TopMenuProps) {
+function TopMenu() {
   const [{ user }] = useStateValue() as any;
-console.log(user.uid)
   return (
     <div className="px-5 py-2 bg-gray-900 text-gray-200">
       <div className="justify-center items-center flex space-x-4 text-gray-400">
@@ -14,30 +12,24 @@ console.log(user.uid)
         <ArrowBack />
         <ArrowForward />
         <QueryBuilder />
-        <TopMenuSearch title={title} />
+        <div className="px-4 w-60 bg-gray-700 rounded-md flex space-x-2 border border-gray-600">
+          <Search />
+          <input className="w-full bg-transparent text-sm outline-none" placeholder="Search..."></input>
+        </div>
         <HelpOutline />
         <div className="flex flex-grow" />
-        {user?.displayName}
-        {user ? <Avatar className="header__avatar"
-          alt={user?.displayName}
-          src={user?.photoURL}
-          style={{ width: "25px", height: "25px" }}
-        ></Avatar> :
-          <PersonOutline />
+        {user && <>
+          <span>{user.displayName}</span>
+          <Avatar className="header__avatar"
+            alt={user?.displayName}
+            src={user?.photoURL}
+            style={{ width: "25px", height: "25px" }}
+          />
+        </>
         }
       </div>
     </div>
   )
-}
-
-interface TopMenuSearchProps { title: string }
-function TopMenuSearch({ title }: TopMenuSearchProps) {
-  return (
-    <div className=" px-4 w-60 bg-gray-700 rounded-md flex space-x-2 border border-gray-600">
-      <Search />
-      <input className="w-full bg-transparent text-sm outline-none" placeholder={`Search ${title}`}></input>
-    </div>
-  );
 }
 
 export default TopMenu
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,18 @@
 
-import React, { useEffect, useState } from 'react'
-import { Route, Switch, useParams } from 'react-router'
+import React from 'react'
+import { Route, Switch } from 'react-router'
 import SideMenu from '../components/SideMenu'
 import TopMenu from '../components/TopMenu'
-import WorkspaceMenu from '../components/WorkspaceMenu'
-import db from '../firebase'
-import QueryParams from '../models/queryParams'
-import { useStateValue } from '../StateProvider'
 import ChannelScreen from './home/ChannelScreen'
 import NotFound from './home/NotFound'
 
 function Home() {
-  let { workspaceID } = useParams<QueryParams>()
-  const [{ user }] = useStateValue() as any;
-  const [workspace, setWorkspace] = useState<any>([]);
-  useEffect(() => {
-    db.collection('workspaces')
-      .onSnapshot(snapshot => (
-        snapshot.docs.forEach((doc) => {
-          if (doc.id === workspaceID) {
-            setWorkspace({
-              id: doc.id,
-              ...doc.data()
-            })
-          }
-        }))
-      )
-  }, [workspaceID, user.uid])
-
   return (
     <header className="font-sans antialiased bg-gray-800 text-gray-200 text-base h-screen flex flex-col space-y-0.5 overflow-hidden">
-      <TopMenu title={workspace.name} />
+      <TopMenu />
       <div className="flex flex-auto overflow-hidden">
         <div className="flex flex-row space-x-0.5">
-          <WorkspaceMenu />
-          <SideMenu title={workspace.name} />
+          <SideMenu />
         </div>
         <Switch>
           <Route path="/workspace/:workspaceID/channel/:channelID"><ChannelScreen /></Route>
