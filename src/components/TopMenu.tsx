6e9cc5f08d17c7c0fc30735d4c1b8aaff326bb13
spiddy <dimitris.kapanidis@harbur.io>
2021-03-25 17:35:25 +0100

    wip

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -6,26 +6,28 @@ import { useStateValue } from '../StateProvider';
 function TopMenu() {
   const [{ user }] = useStateValue() as any;
   return (
-    <div className="justify-center items-center flex space-x-4 text-gray-400">
-      <div className="flex flex-grow " />
-      <ArrowBack />
-      <ArrowForward />
-      <QueryBuilder />
-      <div className="px-4 w-60 bg-gray-700 rounded-md flex space-x-2 border border-gray-600">
-        <Search />
-        <input className="w-full bg-transparent text-sm outline-none" placeholder="Search..."></input>
+    <div className="px-5 py-2 bg-gray-900 text-gray-200">
+      <div className="justify-center items-center flex space-x-4 text-gray-400">
+        <div className="flex flex-grow " />
+        <ArrowBack />
+        <ArrowForward />
+        <QueryBuilder />
+        <div className="px-4 w-60 bg-gray-700 rounded-md flex space-x-2 border border-gray-600">
+          <Search />
+          <input className="w-full bg-transparent text-sm outline-none" placeholder="Search..."></input>
+        </div>
+        <HelpOutline />
+        <div className="flex flex-grow" />
+        {user && <>
+          <span>{user.displayName}</span>
+          <Avatar className="header__avatar"
+            alt={user?.displayName}
+            src={user?.photoURL}
+            style={{ width: "25px", height: "25px" }}
+          />
+        </>
+        }
       </div>
-      <HelpOutline />
-      <div className="flex flex-grow" />
-      {user && <>
-        <span>{user.displayName}</span>
-        <Avatar className="header__avatar"
-          alt={user?.displayName}
-          src={user?.photoURL}
-          style={{ width: "25px", height: "25px" }}
-        />
-      </>
-      }
     </div>
   )
 }
